2629f2635529a368b46c2ee81cbcebbbbc798f7b
jirapatfifa <jirapat_ta66@live.rmutl.ac.th>
2024-03-08 12:29:14 +0700

    edit dbconfig

--- server/dbconfig.js
+++ server/dbconfig.js
@@ -11,10 +11,11 @@ var dbconfig = {
       //connectionLimit : 10,
       host: "localhost",
       port: "3306",
-      user: "dbuser",
-      password: "P@ssw0rd@",
+      user: "dbuser66",
+      password: "P@ssw0rd",
       database: "user66006_db",
     },
   };
+  //แก้ user ให้ถูกต้อง
   module.exports = dbconfig;
   
